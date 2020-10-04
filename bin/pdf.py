69845a3b3cd3b5973d94c518636450f2667e8735
Matthew Dillon <matthewrdillon@gmail.com>
2020-10-04 10:48:06 -0700

    squash

--- bin/pdf.py
+++ bin/pdf.py
@@ -1,6 +1,6 @@
 import io
 
-from PyPDF2 import PdfFileWriter, PdfFileReader
+import PyPDF2
 from reportlab.pdfgen import canvas
 from reportlab.lib import pagesizes
 
@@ -8,16 +8,16 @@ from reportlab.lib import pagesizes
 def write_cheat_sheet(name, username, password):
     bytez = io.BytesIO()
     url = 'https://workshop-server.qiime2.org/%s' % username
+    out_fn = '%s.pdf' % username
 
-    c = canvas.Canvas(bytez, pagesize=pagesizes.letter)
+    c = canvas.Canvas(bytez, pagesize=pagesizes.A4)
     c.setFillColorRGB(1, 1, 1)
 
-    row_1_y = 518
-    row_2_y = 490
-    col_1_x = 25
-    col_2_x = 310
-    col_3_x = 437
+    # You will need to update these, if the layout changes
+    row_1_y, row_2_y = 518, 490
+    col_1_x, col_2_x, col_3_x = 25, 310, 437
 
+    # Write out the dynamic content
     c.drawString(col_1_x, row_1_y, name)
     c.drawString(col_1_x, row_2_y, url)
     c.drawString(col_2_x, row_1_y, username)
@@ -26,20 +26,18 @@ def write_cheat_sheet(name, username, password):
     c.save()
 
     bytez.seek(0)
-    overlay = PdfFileReader(bytez)
+    overlay = PyPDF2.PdfFileReader(bytez)
 
-    with open('sheet.pdf', 'rb') as fh_in:
-        template = PdfFileReader(fh_in)
-
-        output = PdfFileWriter()
+    with open('sheet.pdf', 'rb') as fh_in, open(out_fn, 'wb') as fh_out:
+        template = PyPDF2.PdfFileReader(fh_in)
+        output_pdf = PyPDF2.PdfFileWriter()
         page = template.getPage(0)
         page.mergePage(overlay.getPage(0))
-        output.addPage(page)
-
-        with open('%s.pdf' % username, 'wb') as fh_out:
-            output.write(fh_out)
+        output_pdf.addPage(page)
+        output_pdf.write(fh_out)
 
 
 if __name__ == '__main__':
     # TODO: read roster.csv (or some variant) and iterate
+    # TODO: read in base template argv
     write_cheat_sheet('Matthew Ryan Dillon', 'spicy-lobster', '1234567890')
